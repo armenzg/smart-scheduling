672a3895af43974f79dbefa2bc95de678dbec6e8
Armen Zambrano G <armenzg@mozilla.com>
2020-03-17 16:30:03 -0400

    Hello world script

---
+++ scripts/hello_world/hello_world.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# XXX: Currently we're doing nothing useful with it
+from taskcluster import Secrets
+
+def main():
+    print("Hello world!")
+
+if __name__ == "__main__":
+    main()
