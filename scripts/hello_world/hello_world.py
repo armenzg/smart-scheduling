cdd122dfd404fc8fcb6a47e31e4f09d5433cbbc2
Armen Zambrano G <armenzg@mozilla.com>
2020-03-18 17:01:52 -0400

    Use Taskcluster proxy if running inside of task

--- scripts/hello_world/hello_world.py
+++ scripts/hello_world/hello_world.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
+import os
+
 import taskcluster
 
 def main():
-    secrets = taskcluster.Secrets(taskcluster.optionsFromEnvironment())
+    # Use proxy if configured (within a task), otherwise, use local credentials from env vars
+    if 'TASKCLUSTER_PROXY_URL' in os.environ:
+        options = {'rootUrl': os.environ['TASKCLUSTER_PROXY_URL']}
+    else:
+        options = taskcluster.optionsFromEnvironment()
+    secrets = taskcluster.Secrets(options)
     secret = secrets.get("project/cia/garbage/foo")
     print(secret["secret"])
 
